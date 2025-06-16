0078bd1c0509f6842838493cbf2ff10e8fc1a27a
kmlnrao <kmlnrao2008@gmail.com>
2025-06-16 17:48:55 +0530

    new file added

--- BillingService/Models/BillService.cs
+++ BillingService/Models/BillService.cs
@@ -1,10 +1,15 @@
-﻿public class BillService
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+public class BillService
 {
     public int BillServiceId { get; set; }
     public int BillId { get; set; }
     public int ServiceId { get; set; }
-    public string ServiceName { get; set; } // Redundant but useful
+   // public string ServiceName { get; set; } // Redundant but useful
     public decimal UnitPrice { get; set; }
     public int Quantity { get; set; }
-    public decimal TotalPrice { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public decimal TotalPrice { get; private set; } // Computed by the database
+   
 }
